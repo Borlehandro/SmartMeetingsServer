c7e1f8e4357beb948df90ca24e9e0ebe85b2c2f1
brainworm <borlehandro@gmail.com>
2020-10-07 18:28:10 +0700

    Initial commit

---
+++ src/main/java/com/smartmeetings/server/SmartMeetingsServerApplication.java
@@ -0,0 +1,13 @@
+package com.smartmeetings.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartMeetingsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartMeetingsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartmeetings/server/SmartMeetingsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartmeetings.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartMeetingsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
